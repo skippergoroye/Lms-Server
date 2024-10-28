3282287416c3ba66257a35d8e1a646cf75b5ff50
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-10-28 16:21:19 +0100

    cors re added

--- app.ts
+++ app.ts
@@ -19,12 +19,14 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 // cors => cross origin resource sharing
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true 
-  })
-);
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
+
+
 
 // api requests limit
 const limiter = rateLimit({
